0b696a8577303bc81c948c0f66c0ee6d05c067f2
BlackAce <alfredo.gon195@gmail.com>
2019-09-23 00:20:59 -0700

    added switch cases

--- liri.js
+++ liri.js
@@ -1,15 +1,36 @@
 require("dotenv").config();
 
+var request = require("request");
 var fs = require('fs');
 
 // keys
 var keys = require("./keys.js");
 
 //spotify
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 //input command for gitbash
+var userOption = process.argv[2]; 
 var text = process.argv[3]
+
+//switch
+function userInputs (userOption, text) {
+    switch(userOption) {
+        case "concert-this":
+            concerts(text)
+            break;
+        case "spotify-this-song":
+            spotify(text)
+            break;
+        case "movie-this":
+            movies(text)
+            break;
+        case "do-what-it-says":
+            doWhat(Text)
+            break;
+    }
+}
 
 // if statements
 if (process.argv[2] === "spotify-this-song") {
@@ -23,11 +44,17 @@ if (process.argv[2] === "spotify-this-song") {
 }
 
 // Functions 
+//concerts
+function concerts (text){
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    request(queryUrl, function(error, response, body){
+        
+    })
+}
 
 // spotify
 function spotify() {
 
 }
-//concerts
 //movies
 //doWhat
